b7a9503620c51cf289ff1ea16b4bf074130b3958
Сергей Воротынцев <vorotyncev.si@yandex.ru>
2023-08-29 13:36:16 +0300

    after review

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 
 
+class Genre(models.Model):
+    name = models.CharField('Жанр', max_length=256)
+    slug = models.SlugField('Слаг', unique=True, max_length=50)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
 class Title(models.Model):
     name = models.CharField(max_length=256)
     year = models.PositiveIntegerField('Год выпуска', max_length=4)
     description = models.TextField('Описание', blank=True)
-    genre = models.ManyToManyField('Genre', blank=True, related_name='title')
+    genre = models.ManyToManyField(Genre, blank=True, related_name='title')
     category = models.ForeignKey(
         'Category',
         on_delete=models.SET_NULL,
@@ -34,14 +46,5 @@ class Category(models.Model):
         return self.name
 
 
-class Genre(models.Model):
-    name = models.CharField('Жанр', max_length=256)
-    slug = models.SlugField('Слаг', unique=True, max_length=50)
 
-    class Meta:
-        verbose_name = 'Жанр'
-        verbose_name_plural = 'Жанры'
-
-    def __str__(self):
-        return self.name
 
