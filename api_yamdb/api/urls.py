9982559455b4b55b0650f2eba0f92ceb02591d8b
Afan Pavlov <afonyapav1996@gmail.com>
2023-09-04 14:59:53 +0700

    add UserRegistrationsViewcet

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,12 +2,14 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (CategoryViewSet, GenreViewSet,
-                    TitleViewSet)
+                    TitleViewSet, UserRegistrationViewSet)
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'genres', GenreViewSet)
 router.register(r'titles', TitleViewSet)
+router.register(r'auth/signup', UserRegistrationViewSet)
+router.register(r'users', UserRegistrationViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,10 +1,38 @@
 from django.db.models import Avg
+from django.core.mail import send_mail
+from django.contrib.auth.tokens import default_token_generator
+
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, mixins, status, viewsets
-from reviews.models import Category, Genre, Review, Title
+from rest_framework.response import Response
+from rest_framework import filters, mixins, status, viewsets, permissions
+
+from reviews.models import Category, Genre, Review, Title, User
 from .permissions import IsAdminOrReadOnly
 from .serializers import (CategorySerializer, GenreSerializer, TitleReadSerializer,
-                          TitleSerializer)
+                          TitleSerializer, UserRegistrationSerializer)
+
+
+class UserRegistrationViewSet(mixins.CreateModelMixin,
+                        viewsets.GenericViewSet):
+    """Вьюсет для создания обьекта класса User."""
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def create(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        user, _ = User.objects.get_or_create(**serializer.validated_data)
+        confirmation_code = default_token_generator.make_token(user)
+        send_mail(
+            subject='Код подтверждения',
+            message=f'Ваш код подтверждения: {confirmation_code}',
+            recipient_list=(user.email,),
+            from_email = 'afonyapav@mail.ru'
+        )
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
