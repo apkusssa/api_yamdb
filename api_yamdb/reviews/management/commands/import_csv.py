d83bcafa605a5b1329de4c4f342e16e8db7e9cb5
Afan Pavlov <afonyapav1996@gmail.com>
2023-09-08 00:32:43 +0700

    1

--- api_yamdb/reviews/management/commands/import_csv.py
+++ api_yamdb/reviews/management/commands/import_csv.py
@@ -4,7 +4,8 @@ import os
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from reviews.models import Category, Comment, Genre, GenreTitle, Review, Title, User
+from reviews.models import (Category, Comment, Genre,
+                            GenreTitle, Review, Title, User)
 
 
 def category_create(row):
